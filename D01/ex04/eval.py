9400075db362befb6d2a54ce2b1623c1c14974a7
Adrien Vardon <adrienvardon@Admins-MacBook-Pro-2.local>
2019-10-23 10:12:40 +0200

    Add D01 ex04 and ex05

--- D01/ex04/eval.py
+++ D01/ex04/eval.py
@@ -2,8 +2,6 @@ class Evaluator:
     def __init__(self):
         pass
 
-    
-
     #def zip_evaluate(coefs, words):
     #    res = 0
     #    if type (coefs) is list and type (words) is list:
@@ -22,7 +20,7 @@ class Evaluator:
             tmp = list(zip(words))
             for i in range(len(coefs)):
                 res += coefs[i] * len(tmp[i][0])
-            print (res)
+            return res
 
 # The zip function in Python 3 returns an iterator. Iterators can only be exhausted (by something like making a list out of them) once.
 # The purpose of this is to save memory by only generating the elements of the iterator as you need them, rather than putting it all into memory at once.
@@ -43,10 +41,23 @@ class Evaluator:
     #    print(res)
 
     @staticmethod
-    def enumerate_evaluate():
-        pass
+    def enumerate_evaluate(coefs, words):
+        res = 0
+        if type (coefs) is list and type (words) is list:
+            if len(coefs) != len(words):
+                return -1
+            for i, value in enumerate(words):
+                res += len(value) * coefs[i]
+            return res
+
+        #should use yield:
+        #https://docs.python.org/fr/3/library/functions.html#enumerate
 
 if __name__ == "__main__":
     words = ["Le", "Lorem", "Ipsum", "est", "simple"]
     coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
-    Evaluator.zip_evaluate(coefs, words)
+    print(Evaluator.zip_evaluate(coefs, words))
+
+    words = ["Le", "Lorem", "Ipsum", "n'", "est", "pas", "simple"]
+    coefs = [0.0, -1.0, 1.0, -12.0, 0.0, 42.42]
+    print (Evaluator.enumerate_evaluate(coefs,words))
---
+++ D01/ex05/the_bank.py
@@ -0,0 +1,51 @@
+class Account(object):
+    ID_COUNT = 1
+    value = 0
+    def __init__(self, name, **kwargs):
+        self.id = self.ID_COUNT
+        self.name = name
+        self.__dict__.update(kwargs)
+        if hasattr(self, 'value'):
+            self.value = 0
+        Account.ID_COUNT += 1
+    def transfer(self, amount):
+        self.value += amount
+
+#kwargs? == #add value = 0 in class Account?
+#hasattr?
+#ID_COUNT before init = static in C?
+
+
+class Bank(object):
+    """The bank"""
+    def __init__(self):
+        self.account = []
+    def add(self, account):
+        self.account.append(account)
+    def transfer(self, origin, dest, amount):
+        """
+            @origin: int(id) or str(name) of the first account
+            @dest:    int(id) or str(name) of the destination account
+            @amount: float(amount) amount to transfer
+            @return         True is success, False is an error occured
+        """
+        if origin == Account and dest == Account and type(amount) is int or float and amount > 0:
+            if origin.value > amount :
+                origin.value -= amount
+                dest.value += amount
+    def fix_account(self, account):
+        """
+            fix the corrupted account
+            @account: int(id) or str(name) of the account
+            @return         True is success, False is an error occured
+        """
+
+if __name__ == "__main__":
+    client1 = Account("client1")
+    client2 = Account("client2")
+    client1.transfer(1000)
+    client2.transfer(250)
+    JPMorgan = Bank()
+    JPMorgan.transfer(client1, client2, 200)
+    print(client1.value)
+    print(client2.value)
